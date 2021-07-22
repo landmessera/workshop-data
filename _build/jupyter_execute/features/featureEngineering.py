6a0b5f87b77c4774ede64a7756b8705257d8b0ad
Annette Landmesser <landmessera@hdm-stuttgart.de>
2021-07-22 12:41:50 +0200

    removed files and folders which are not relevant

--- _build/jupyter_execute/features/featureEngineering.py
+++
@@ -1,5 +0,0 @@
-# Merkmalsoptimierung
-
-In den vorigen Abschnitten wurde eine Basis für die Anwendung von Machine Learning Verfahren geschaffen und ein erstes Ergebnis erzielt. Es wurden auch erste Optimierungen durchgeführt, die sich auf die Parameter der Transformationen und Hyperparameter der Machine Learning Algorithmen konzentrierten. In diesem Abschnitt wird auf die Optimierung in Bezug auf die Merkmale eingegangen. In den meisten Anwendungsfällen bringt diese Art von Optimierung - wenn Sie qualitativ hochwertig ist - die größte Leistungssteigerung. Bei der Merkmalsoptimierung ist häufig eine gute Kenntnis der Domäne erforderlich. 
-
-Auch im Umfeld der Merkmalsoptimierung existieren eine Vielzahl von Methoden. Es werden eine kleine Auswahl vorgestellt um ein Verständnis des Vorgehens zu erlangen.
